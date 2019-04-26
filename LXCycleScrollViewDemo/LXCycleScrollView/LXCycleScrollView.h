d89b30f22bbe8b3295f639dde17b669cea4e2be4
heyong <hy@sasucen.com>
2019-04-26 09:44:43 +0800

    first commit

---
+++ LXCycleScrollViewDemo/LXCycleScrollView/LXCycleScrollView.h
@@ -0,0 +1,17 @@
+//
+//  LXCycleScrollView.h
+//  LXCycleScrollViewDemo
+//
+//  Created by heyong on 2019/4/25.
+//  Copyright © 2019年 liuxing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LXCycleScrollView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LXCycleScrollViewDemo/LXCycleScrollView/LXCycleScrollView.m
@@ -0,0 +1,21 @@
+//
+//  LXCycleScrollView.m
+//  LXCycleScrollViewDemo
+//
+//  Created by heyong on 2019/4/25.
+//  Copyright © 2019年 liuxing. All rights reserved.
+//
+
+#import "LXCycleScrollView.h"
+
+@implementation LXCycleScrollView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
